50818917635d9e73ba4c31269367127d8ee7008b
Alf <42708476+alfset@users.noreply.github.com>
2025-06-24 12:52:16 +0700

    Create user.go

---
+++ auth/user.go
@@ -0,0 +1,10 @@
+package auth
+
+var Users = map[string]string{
+    "user1": "token-user1",
+    "custody1": "token-custody1",
+}
+
+var CustodyMap = map[string]string{
+    "user1": "custody1",
+}
